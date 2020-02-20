f1ab119aabccd79e90e8117224b755ff2cebb59d
hyun <coderhyun476@gmail.com>
2020-02-20 22:23:43 +0900

    connet react-navigation

---
+++ app/src/screens/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from './HomeScreen';
+import ChatScreen from './ChatScreen';
+
+const Stack = createStackNavigator()
+
+const AppContainer = () =>
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen
+                name='Home'
+                component={HomeScreen}
+            />
+            <Stack.Screen
+                name='Chat'
+                component={ChatScreen}
+            />
+        </Stack.Navigator>
+    </NavigationContainer>
+
+export default AppContainer
